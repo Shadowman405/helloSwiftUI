c558a3650df620657724d4dc64359657a74a7c95
m_mitin <maximus405@gmail.com>
2022-08-24 20:07:34 +0300

    Initial Commit

---
+++ helloSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloSwiftUI
+//
+//  Created by Maxim Mitin on 24.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloSwiftUI/helloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloSwiftUIApp.swift
+//  helloSwiftUI
+//
+//  Created by Maxim Mitin on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct helloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
